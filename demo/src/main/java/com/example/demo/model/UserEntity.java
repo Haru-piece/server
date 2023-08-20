a744d857ecec887b6a729acaca4d10536b4c3ed3
kim <kimm240@naver.com>
2023-08-20 22:34:12 +0900

    .git 수정

--- demo/src/main/java/com/example/demo/model/UserEntity.java
+++ demo/src/main/java/com/example/demo/model/UserEntity.java
@@ -36,7 +36,7 @@ public class UserEntity {
 	private String username; // 유저의 이름
 
 	@Column(nullable = false)
-	private String email; // 유저의 email, 아이디와 같은 기능을 한다.
+	private String email; // 유저의 email, 아이디와 같은 기능을 한다..
 
 	@Column(nullable = false)
 	private String password; // 패스워드
